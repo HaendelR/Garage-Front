db32c4ddb7eb54f15c36b97a3737ff71f2179b90
HaendelR <89445756+HaendelR@users.noreply.github.com>
2023-01-21 13:26:52 +0300

    Drag and drop
    reparation car

--- m1p10mean-Mendrika-Haendel/src/app/app-routing.module.ts
+++ m1p10mean-Mendrika-Haendel/src/app/app-routing.module.ts
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AccueilComponent } from './pages/accueil/accueil.component';
-import { AtelierAccueilComponent } from './pages/atelier-accueil/atelier-accueil.component';
-import { ClientAccueilComponent } from './pages/client-accueil/client-accueil.component';
-import { DiagnostiqueDevisComponent } from './pages/diagnostique-devis/diagnostique-devis.component';
-import { FormDiagnosticComponent } from './pages/form-diagnostic/form-diagnostic.component';
-import { InscriptionComponent } from './pages/inscription/inscription.component';
-import { VoitureDiagnostiquesAtelierComponent } from './pages/voiture-diagnostiques-atelier/voiture-diagnostiques-atelier.component';
-import { VoitureDiagnostiquesComponent } from './pages/voiture-diagnostiques/voiture-diagnostiques.component';
-=======
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AccueilComponent } from "./pages/accueil/accueil.component";
@@ -19,7 +7,6 @@ import { DragCarComponent } from "./pages/drag-car/drag-car.component";
 import { FormDiagnosticComponent } from "./pages/form-diagnostic/form-diagnostic.component";
 import { InscriptionComponent } from "./pages/inscription/inscription.component";
 import { VoitureDiagnostiquesComponent } from "./pages/voiture-diagnostiques/voiture-diagnostiques.component";
->>>>>>> Stashed changes
 
 const routes: Routes = [
   {
@@ -44,25 +31,12 @@ const routes: Routes = [
   },
   {
     path: "voiture-diagnostiques",
-<<<<<<< Updated upstream
-    component: VoitureDiagnostiquesComponent
-  },
-  {
-    path: "devis-diagnostique/:numberPlate/:status",
-    component: DiagnostiqueDevisComponent
-  },
-  {
-    path: "voiture-diagnostiques-atelier",
-    component: VoitureDiagnostiquesAtelierComponent
-  }
-=======
     component: VoitureDiagnostiquesComponent,
   },
   {
     path: "drag-car",
     component: DragCarComponent,
   },
->>>>>>> Stashed changes
 ];
 
 @NgModule({
--- m1p10mean-Mendrika-Haendel/src/app/app.module.ts
+++ m1p10mean-Mendrika-Haendel/src/app/app.module.ts
@@ -4,18 +4,6 @@ import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
-<<<<<<< Updated upstream
-import { AppComponent } from './app.component';
-import { AccueilComponent } from './pages/accueil/accueil.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
-import { ClientAccueilComponent } from './pages/client-accueil/client-accueil.component';
-import { AtelierAccueilComponent } from './pages/atelier-accueil/atelier-accueil.component';
-import { InscriptionComponent } from './pages/inscription/inscription.component';
-import { FormDiagnosticComponent } from './pages/form-diagnostic/form-diagnostic.component';
-import { VoitureDiagnostiquesComponent } from './pages/voiture-diagnostiques/voiture-diagnostiques.component';
-import { DiagnostiqueDevisComponent } from './pages/diagnostique-devis/diagnostique-devis.component';
-import { VoitureDiagnostiquesAtelierComponent } from './pages/voiture-diagnostiques-atelier/voiture-diagnostiques-atelier.component';
-=======
 import { AppComponent } from "./app.component";
 import { AccueilComponent } from "./pages/accueil/accueil.component";
 import { NavbarComponent } from "./pages/navbar/navbar.component";
@@ -26,7 +14,6 @@ import { FormDiagnosticComponent } from "./pages/form-diagnostic/form-diagnostic
 import { VoitureDiagnostiquesComponent } from "./pages/voiture-diagnostiques/voiture-diagnostiques.component";
 import { DragCarComponent } from "./pages/drag-car/drag-car.component";
 import { DragDropModule } from "@angular/cdk/drag-drop";
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -38,12 +25,7 @@ import { DragDropModule } from "@angular/cdk/drag-drop";
     InscriptionComponent,
     FormDiagnosticComponent,
     VoitureDiagnostiquesComponent,
-<<<<<<< Updated upstream
-    DiagnostiqueDevisComponent,
-    VoitureDiagnostiquesAtelierComponent,
-=======
     DragCarComponent,
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
