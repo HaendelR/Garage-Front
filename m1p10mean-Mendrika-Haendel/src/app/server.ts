193711826e4b22221fabcca6ab519a98652ea52a
HaendelR <89445756+HaendelR@users.noreply.github.com>
2023-01-28 14:42:31 +0300

    .env

--- m1p10mean-Mendrika-Haendel/src/app/server.ts
+++ m1p10mean-Mendrika-Haendel/src/app/server.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment";
 @Injectable({
   providedIn: "root",
 })
 export class Server {
   getUrl(): String {
-    const url = `http://localhost:3000`;
+    const url = environment.API_URL;
     return url;
   }
 }
